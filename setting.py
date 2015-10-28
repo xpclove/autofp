aee3130c587ca400ea8ac47ab378ae7d26c97919
xpc <xpclove@126.com>
2015-10-28 22:44:28 +0800

    setting JSON

--- setting.py
+++ setting.py
@@ -1,5 +1,6 @@
 import os
 import run
+import json 
 ########################################################################
 class setting:
     """"""
@@ -12,6 +13,7 @@ class setting:
     Atom_Aniso_Temp=True
     rm_tmp_done=False
     origin_path="";
+    setjson={};
     output={"Hkl":False,
             "Cif":False,
             "Fou":False
@@ -22,6 +24,7 @@ class setting:
         """Constructor"""
     def load_setting(self,path="setting.txt"):
         setting_file=open(path,"r")
+        '''
         context_set=setting_file.readlines()
         context=[]
         i=""
@@ -29,18 +32,30 @@ class setting:
             value=i.split('=')[1]
             context.append(value)
         
+        
+        
         setting_file.close()
         i=0
         self.show_rwp=(bool)(context[i]);i+=1;
         self.show_log_FP=(bool)(context[i]);i+=1;
         self.AsymLim=(int)(context[i]);i+=1;
         self.origin_path=context[i];i+=1;
+        '''
+        self.setjson=json.load(setting_file)
+        setting_file.close()
+        self.show_rwp=self.setjson["show_rwp"]
+        self.show_log_FP=self.setjson["show_log_FP"]
+        self.AsymLim=self.setjson["AsymLim"]
+        self.origin_path=self.setjson["origin_path"]
         return
     def save_setting(self,path="setting.txt"):
         setting_file=open(path,"w")
+        '''
         setting_file.write(str(self.show_rwp)+"\n")
         setting_file.write(str(self.show_log_FP)+"\n")
         setting_file.write(str(self.AsymLim)+"\n")
+        '''
+        json.dump(self.setjson,setting_file,indent=1)
         setting_file.close()
 run_set=setting()        
         
